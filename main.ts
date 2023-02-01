9847eda2cbd516b4c11c8bedaff14cabbe0cd8e0
Kindtb <bart.kindt@gmail.com>
2023-02-01 08:39:25 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -6,5 +6,13 @@ basic.showLeds(`
     # . . . .
     `)
 basic.forever(function () {
-	
+    if (pins.digitalReadPin(DigitalPin.P0) == 1) {
+        basic.showLeds(`
+            # . . # .
+            # . . # .
+            # . . # .
+            # . . # .
+            # . . # .
+            `)
+    }
 })
